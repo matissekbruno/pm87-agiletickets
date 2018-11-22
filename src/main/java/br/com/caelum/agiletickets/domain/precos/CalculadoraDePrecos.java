8cb4d396914926e29df85bd9104af54c20717d3c
agil7990 <agil7990@caelumBSB03-01>
2018-11-22 10:40:34 -0200

    Refatoracao de extracao de variaveis e metodos da Classe CalculadoraDePrecos, para melhorar a legibilidade da classe

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -6,36 +6,53 @@ import br.com.caelum.agiletickets.models.Sessao;
 import br.com.caelum.agiletickets.models.TipoDeEspetaculo;
 
 public class CalculadoraDePrecos {
+	
+	private static final double PORCENTAGEM_ULTIMOS_INGRESSOS_CINEMA = 0.05;
+	private static final double PORCENTAGEM_ULTIMOS_INGRESSOS_SHOW = 0.05;
+	private static final double PORCENTAGEM_ULTIMOS_INGRESSOS_BALLET = 0.50;
+	private static final double PORCENTAGEM_ULTIMOS_INGRESSOS_ORQUESTRA = 0.50;
 
+	private static final BigDecimal MULTIPLICADOR_PRECO_ULTIMOS_INGRESSOS_CINEMA = BigDecimal.valueOf(0.10);
+	private static final BigDecimal MULTIPLICADOR_PRECO_ULTIMOS_INGRESSOS_SHOW = BigDecimal.valueOf(0.10);
+	private static final BigDecimal MULTIPLICADOR_PRECO_ULTIMOS_INGRESSOS_BALLET = BigDecimal.valueOf(0.20);
+	private static final BigDecimal MULTIPLICADOR_PRECO_ULTIMOS_INGRESSOS_ORQUESTRA = BigDecimal.valueOf(0.20);
+	
+	private static final BigDecimal MULTIPLICADOR_PRECO_DURACAO_SUPERIOR_UMA_HORA_BALLET = BigDecimal.valueOf(0.10);
+	private static final BigDecimal MULTIPLICADOR_PRECO_DURACAO_SUPERIOR_UMA_HORA_ORQUESTRA = BigDecimal.valueOf(0.10);
+	
+	public static double calculaPorcentagemIngressosRestantes(Sessao sessao) {
+		return (sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue();
+	}
+	
 	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {
 		BigDecimal preco;
 		
 		if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.CINEMA) || sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.SHOW)) {
 			//quando estiver acabando os ingressos... 
-			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.05) { 
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
+			if(calculaPorcentagemIngressosRestantes(sessao) <= PORCENTAGEM_ULTIMOS_INGRESSOS_CINEMA) { 
+				preco = sessao.getPreco().add(sessao.getPreco().multiply(MULTIPLICADOR_PRECO_ULTIMOS_INGRESSOS_CINEMA));
 			} else {
 				preco = sessao.getPreco();
 			}
 		} else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.BALLET)) {
-			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.50) { 
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
+			if(calculaPorcentagemIngressosRestantes(sessao) <= PORCENTAGEM_ULTIMOS_INGRESSOS_BALLET) { 
+				preco = sessao.getPreco().add(sessao.getPreco().multiply(MULTIPLICADOR_PRECO_ULTIMOS_INGRESSOS_BALLET));
 			} else {
 				preco = sessao.getPreco();
 			}
 			
 			if(sessao.getDuracaoEmMinutos() > 60){
-				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
+				preco = preco.add(sessao.getPreco().multiply(MULTIPLICADOR_PRECO_DURACAO_SUPERIOR_UMA_HORA_BALLET));
 			}
 		} else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.ORQUESTRA)) {
-			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.50) { 
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
+			if(calculaPorcentagemIngressosRestantes(sessao) <= PORCENTAGEM_ULTIMOS_INGRESSOS_ORQUESTRA) { 
+				preco = sessao.getPreco().add(sessao.getPreco().multiply(MULTIPLICADOR_PRECO_ULTIMOS_INGRESSOS_ORQUESTRA));
 			} else {
 				preco = sessao.getPreco();
 			}
 
 			if(sessao.getDuracaoEmMinutos() > 60){
-				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
+				preco = preco.add(sessao.getPreco().multiply(MULTIPLICADOR_PRECO_DURACAO_SUPERIOR_UMA_HORA_ORQUESTRA));
 			}
 		}  else {
 			//nao aplica aumento para teatro (quem vai é pobretão)
